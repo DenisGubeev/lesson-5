38d3a96a0a05d901fa0b94a3d596871245f0db9c
DenisGubeev <35633718+DenisGubeev@users.noreply.github.com>
2018-02-08 05:15:16 +0300

    Update and rename table.php to index.php

--- index.php
+++ index.php
@@ -1,26 +1,27 @@
 <?php
 $data = file_get_contents('data.json');
 $persons = json_decode($data, true);
-
 ?>
-
 
 <html>
 <head>
 <style>
-
+	.border{
+		border: 3px solid gray;
+		border-collapse: collapse;
+	}
 </style>
 </head>
 <body>
 	<table>
-		<tr class="">
+		<tr class="border">
 			<td>Имя</td>
 			<td>Фамилия</td>
 			<td>Адрес</td>
 			<td>Телефон</td>
 		</tr>
 		<?php foreach($persons as $person) : ?>
-			<tr class="">
+			<tr class="border">
 				<td><?php echo $person['firstName']; ?></td>
 				<td><?php echo $person['lastName']; ?></td>
 				<td><?php echo $person['address']; ?></td>
